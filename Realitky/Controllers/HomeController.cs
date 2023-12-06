d31d2f983313232399ca85527ecc5d92eb51431c
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-12-06 20:34:47 +0100

    Structure + Css import V1

--- Realitky/Controllers/HomeController.cs
+++ Realitky/Controllers/HomeController.cs
@@ -2,31 +2,38 @@
 using Realitky.Models;
 using System.Diagnostics;
 
-namespace Realitky.Controllers
+namespace Realitky.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index() //Home
+    {
+        return View();
+    }
+    public IActionResult Catalog()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    /*--------------------------------*/
+    public IActionResult Detail()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
