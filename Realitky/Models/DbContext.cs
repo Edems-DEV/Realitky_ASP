58764593a17e6b3ac425cf31607d890b7d814360
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-12-08 19:04:57 +0100

    🛠️ Images
    <3
    wwwroot

---
+++ Realitky/Models/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Buffers.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication4.Models
+{
+    public class MyContext : DbContext
+    {
+        // public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string name = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String("RG9sYW5za3lBZGFt"));
+            optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;" +
+                                    $"database={name}_db1;" +
+                                    $"user={name};" +
+                                    "password=123456;" +
+                                    "SslMode=none");
+        }
+    }
+}
