b717393a73900a49c863debb9a96a3a87f52c3e9
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-12-09 10:37:47 +0100

    🆕Catalog - Filter (0.5)
    TODO:
    - replace href with asp-
    - pre-populate the input fields

--- Realitky/Controllers/HomeController.cs
+++ Realitky/Controllers/HomeController.cs
@@ -36,9 +36,39 @@ public class HomeController : Controller
         
         return View();
     }
-    public IActionResult Catalog()
+    public IActionResult Catalog(int type = 10, int region = 0, int minP = 0, int maxP = 0, int minS = 0, int maxS = 0)
     {
-        this.ViewBag.Offers = this.context.Offers.ToList();
+        var offersQuery = this.context.Offers.AsQueryable();
+
+        // Apply filters only if they are not their default values
+        if (type != 10)
+            offersQuery = offersQuery.Where(o => o.IdType == type);
+        if (region != 0)
+            offersQuery = offersQuery.Where(o => o.IdRegion == region);
+        if (minP != 0)
+            offersQuery = offersQuery.Where(o => o.price >= minP);
+        if (maxP != 0)
+            offersQuery = offersQuery.Where(o => o.price <= maxP);
+        if (minS != 0)
+            offersQuery = offersQuery.Where(o => o.size >= minS);
+        if (maxS != 0)
+            offersQuery = offersQuery.Where(o => o.size <= maxS);
+        
+        //Load based on filters
+        this.ViewBag.Offers = offersQuery.ToList();
+
+        //Load data
+        this.ViewBag.Regions = this.context.Region.ToList();
+        this.ViewBag.Types = this.context.Type.ToList();
+        
+        //ViewBag
+        @ViewBag.Type = type;
+        @ViewBag.Region = region;
+        @ViewBag.MinP = minP;
+        @ViewBag.MaxP = maxP;
+        @ViewBag.MinS = minS;
+        @ViewBag.MaxS = maxS;
+        
         return View();
     }
     public IActionResult Contact()
